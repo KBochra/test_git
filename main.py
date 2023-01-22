77c19308d36970cdfb000519c99881afc20ba036
KBochra <kbochra14@gmail.com>
2023-01-22 17:51:55 +0100

    [Other] Added to message.

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm is beautiful.')
+    print_hi('PyCharm is beautiful. Yes it is.')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
